ca9a578384ce0ec8eeb8c7027888549b83e66b4b
Faizoo <faizaan3424@gmail.com>
2021-12-29 15:37:14 -0500

    sucessful sign up form with loading screen

--- lib/welcome/sign_in.dart
+++ lib/welcome/sign_in.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:my_voice_app/models/loading.dart';
+import 'package:my_voice_app/services/auth.dart';
 import 'package:provider/provider.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 import '../main.dart';
-import './splashscreen.dart';
-import './login_row.dart';
-import './sign_in_form.dart';
+import '../home/home_widget.dart';
+import 'form_decor.dart';
+import 'login_row.dart';
+import 'splashscreen.dart';
 
 class MVSignIn extends StatefulWidget {
   @override
@@ -14,17 +17,25 @@ class MVSignIn extends StatefulWidget {
 }
 
 class _MVSignInState extends State<MVSignIn> {
-  bool isLoading = false;
+  bool _isLoading = false;
 
-  void _refresh(bool loadingState) {
-    setState(() {
-      isLoading = loadingState;
-    });
+  final _formKey = GlobalKey<FormState>();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  late bool _passwordVisible;
+
+  String _errorMessage = "";
+
+  @override
+  void initState() {
+    _passwordVisible = false;
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return isLoading
+    return _isLoading
         ? MVLoading()
         : Scaffold(
             resizeToAvoidBottomInset: false,
@@ -78,9 +89,115 @@ class _MVSignInState extends State<MVSignIn> {
                         children: [
                           LoginRow(signedIn: true, registered: false),
                           SizedBox(height: 20),
-                          SIForm(
-                            refreshParent: _refresh,
-                          ),
+                          Stack(children: [
+                            SizedBox(
+                              height: Provider.of<MVProvider>(context)
+                                      .screenHeightAppbarless /
+                                  3.0,
+                              width:
+                                  Provider.of<MVProvider>(context).screenWidth *
+                                      0.90,
+                              child: Form(
+                                key: _formKey,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    TextFormField(
+                                        validator: (_) => !RegExp(
+                                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                                .hasMatch(_emailController.text)
+                                            ? "Please enter a valid email!"
+                                            : null,
+                                        controller: _emailController,
+                                        keyboardType:
+                                            TextInputType.emailAddress,
+                                        decoration: formDecoration.copyWith(
+                                            hintText: "Email")),
+                                    TextFormField(
+                                      validator: (_) =>
+                                          _passwordController.text.isEmpty
+                                              ? "Please enter a password!"
+                                              : null,
+                                      controller: _passwordController,
+                                      keyboardType: TextInputType.text,
+                                      obscureText: !_passwordVisible,
+                                      decoration: formDecoration.copyWith(
+                                        hintText: "Password",
+                                        suffixIcon: IconButton(
+                                          icon: Icon(
+                                            _passwordVisible
+                                                ? Icons.visibility
+                                                : Icons.visibility_off,
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              _passwordVisible =
+                                                  !_passwordVisible;
+                                            });
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Container(
+                                      width: Provider.of<MVProvider>(context)
+                                              .screenWidth *
+                                          0.90,
+                                      height: 60.0,
+                                      decoration: BoxDecoration(
+                                        color: HexColor("FCB831"),
+                                        border: Border.all(color: Colors.grey),
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(50)),
+                                      ),
+                                      child: TextButton(
+                                        onPressed: () async {
+                                          if (_formKey.currentState!
+                                              .validate()) {
+                                            setState(() {
+                                              _isLoading = true;
+                                            });
+                                            dynamic user =
+                                                await MVAuth.signInEP(
+                                                    _emailController.text,
+                                                    _passwordController.text);
+                                            if (user == null) {
+                                              _isLoading = false;
+                                              setState(() {
+                                                _errorMessage =
+                                                    "Could not sign in. Please try again.";
+                                              });
+                                            } else
+                                              Navigator.of(context)
+                                                  .pushReplacement(
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              MVHome()));
+                                          }
+                                        },
+                                        child: Text(
+                                          "Create account",
+                                          style: TextStyle(
+                                            fontSize: 18.0,
+                                            color: Colors.grey[800],
+                                            fontWeight: FontWeight.w200,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Text(
+                                      _errorMessage,
+                                      style: TextStyle(
+                                          color: Colors.red, fontSize: 12.0),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ])
                         ]),
                   ),
                 ),

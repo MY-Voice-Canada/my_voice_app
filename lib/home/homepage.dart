94caacc538c34daa4212cf7ddae6ab394e06cd4d
Faizoo <faizaan3424@gmail.com>
2021-12-31 18:24:59 -0500

    successfully pull articles from wp website

--- lib/home/homepage.dart
+++ lib/home/homepage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:my_voice_app/models/dummy_article.dart';
 import 'package:my_voice_app/models/user.dart';
+import 'package:my_voice_app/services/wp.dart';
 import 'package:provider/provider.dart';
 
 import '../main.dart';
@@ -18,15 +20,47 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final MVUser user = Provider.of<MVUser?>(context)!;
 
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text("Hello, " + user.displayName, style: TextStyle(fontSize: 48)),
-          Divider(color: Colors.black,),
-          TempArticle(),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text("Hello, ${user.displayName}!", style: TextStyle(fontSize: 48)),
+        Divider(
+          color: Colors.black,
+        ),
+        FutureBuilder<List>(
+          future: MVWP.getAllPosts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return snapshot.data?.length == 0
+                  ? Center(child: Text("No posts available at the moment."))
+                  : Container(
+                      height: 600.0,
+                      child: ListView.builder(
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (context, i) => Card(
+                          child: ListTile(
+                            leading: Image.network(snapshot.data![i]
+                                    ["_embedded"]["wp:featuredmedia"][0]
+                                ["source_url"]),
+                            title: Html(
+                                data: "<h2>" +
+                                    snapshot.data![i]["title"]["rendered"]
+                                        .toString() +
+                                    "</h2>"),
+                            subtitle: Html(
+                                data: snapshot.data![i]["content"]["rendered"]
+                                    .toString()),
+                          ),
+                        ),
+                      ),
+                    );
+            } else
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+          },
+        ),
+      ],
     );
   }
 }
---
+++ lib/services/wp.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class MVWP {
+  static const String _baseUrl =
+      "http://myvoicecanada.com/wp-json/wp/v2/posts?per_page=100&_embed";
+
+  static Future<List> getAllPosts() async {
+    try {
+      var response = await http.get(Uri.parse(_baseUrl));
+      print(jsonDecode(response.body));
+      if (response.statusCode == 200)
+        return jsonDecode(response.body);
+      else
+        return Future.error("Server error");
+    } catch (e) {
+      print(e.toString());
+      return Future.error("Error fetching data");
+    }
+  }
+}
